578b2f63287322cf138399dbf35751af2f6c42f7
saramaenza <61665335+saramaenza@users.noreply.github.com>
2023-06-08 11:44:58 +0200

    2 files modified, 1 added

---
+++ src/root/city.ts
@@ -0,0 +1,8 @@
+export class TempCity {
+  nome: string;
+  valore: string;
+  constructor(nome: string, valore: string) {
+    this.nome = nome;
+    this.valore = valore;
+  }
+}
--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, VERSION } from '@angular/core';
+import { TempCity } from './city';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css'],
   standalone: true,
+  imports: [CommonModule],
 })
 export class RootComponent implements OnInit {
   title: string = 'Temperature in Angular ' + VERSION.major;
+  cities: Array<TempCity> = [
+    new TempCity('Torino', '14'),
+    new TempCity('Milano', '15'),
+    new TempCity('Genova', '18'),
+  ];
+  seleziona(name: string) {
+    this.selezionata = name;
+  }
+  selezionata: string | undefined;
+
+  clean() {
+    this.selezionata = undefined;
+  }
+
+  seleziona2(name: string) {
+    var trovato: Array<TempCity> = this.cities.filter(
+      (el) => el.nome === name //criterio per la ricerca. Ottengo un array di oggetti
+    );
+    this.selezione = trovato[0]; //ottengo l’oggetto in prima posizione, che è anche l’unico, le città sono univoche
+  }
+  selezione: TempCity | undefined;
+
+  clean2() {
+    this.selezione = undefined;
+  }
+
   constructor() {}
 
   ngOnInit() {}
